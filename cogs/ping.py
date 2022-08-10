2d5e473cfc4a0986a5ea18f7c4dba42052a40845
Xeift <80938768+Xeift@users.noreply.github.com>
2022-08-10 17:02:26 +0800

    .

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,14 +1,17 @@
 import discord
 from discord.commands import slash_command
 from discord.ext import commands
-from core.cog_core import cogcore
 
-class ping(cogcore):
+class ping(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+
     @slash_command(guild_ids=[936236815545954384],name='ping',description='return bot latency')
     async def ping(
         self,
         ctx: discord.ApplicationContext
     ):
         await ctx.respond(f"pong! ({self.bot.latency*1000:.2f} ms)")
-def setup(bot):# add cog
+def setup(bot):
     bot.add_cog(ping(bot))
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import discord
+# import asyncio
 
-bot = discord.Bot(intents=discord.Intents.all())
+bot = discord.Bot(intents=discord.Intents.all(),)
+
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    print(f'We have logged in as {bot.user}')
+    # while True:
+    #     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='A nice game'))
+    #     await asyncio.sleep(60)
+
 
 extensions = [# load cogs
-  'cogs.ping'
+    'cogs.ping',
 ]
 
-if __name__ == '__main__':# import cogs from cogs folder
-	for extension in extensions:
-		bot.load_extension(extension) 
-        
-bot.run("YOUR_BOT_TOKEN_FROM_DEV_PORTAL")# bot token
+if __name__ == '__main__': # import cogs from cogs folder
+    for extension in extensions:
+        bot.load_extension(extension)
+
+# bot.run('YOUR_BOT_TOKEN_FROM_DEV_PORTAL')  # bot token
+bot.run('OTMxMzkzNTQxMDk4MzExNzEz.GrZGLV.ee-eq4dxpEXWYERQaiG0zj7hpjIJfkfjDEHSSA')  # bot token
